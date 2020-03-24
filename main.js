844a8aa620679de6fa42649341f52ffd65519fc8
Colin Jaffe <balloonasaurus@gmail.com>
2020-03-23 21:36:57 -0400

    Adds more schema.

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 const _makeStudent = (name) => ({
   name,
-  partners: [],
+  partnerCounts: [],
 });
 
 
 const makeRoster = () => ({
   _students: [],
+  _groups: [],
 
-  addStudent: function(name) {
-    this._students.push(_makeStudent(name));
-  }
+  addStudent: function(newName) {
+    if (this._students.some(({name}) => name === newName)) {
+      throw new Error();
+    }
+
+    if (newName === '') {
+      throw new Error();
+    }
+
+    this._students.push(_makeStudent(newName));
+  },
+
+  removeStudent: function(nameToRemove) {
+    if (this._students.every(({name}) => name !== nameToRemove)) {
+      throw new Error();
+    }
+
+    this._students = this._students.filter(({name}) => name !== nameToRemove);
+  },
 })
 
 
--- main.test.js
+++ main.test.js
@@ -4,46 +4,6 @@ const {
 } = require('./main.js')
 
 
-describe('makeRoster', () => {
-  it(`returns an object`, () => {
-    expect(typeof makeRoster()).toBe('object')
-  })
-})
-
-describe('roster.students', () => {
-  it(`is an empty array`, () => {
-    const students = makeRoster()._students
-    expect(Array.isArray(students)).toBe(true)
-  })
-
-  it(`begins as an empty array`, () => {
-    const students = makeRoster()._students;
-    expect(students.length).toBe(0);
-  })
-})
-
-describe('roster.addStudent', () => {
-  it(`is a method`, () => {
-    const roster = makeRoster();
-    expect(typeof roster.addStudent).toBe('function');
-  })
-
-  it(`adds a student with the given name to the roster`, () => {
-    const roster1 = makeRoster();
-    roster1.addStudent('Colin');
-    expect(typeof roster1._students[0]).toBe('object')
-    expect(roster1._students[0].name).toBe('Colin');
-  })
-
-  it(`adds the student at the end of the roster`, () => {
-    const roster1 = makeRoster();
-    roster1.addStudent('Colin');
-    expect(roster1._students[0].name).toBe('Colin');
-    roster1.addStudent('Maya');
-    expect(roster1._students[1].name).toBe('Maya');
-  })
-})
-
 describe('_makeStudent', () => {
   it(`returns an object`, () => {
     expect(typeof _makeStudent()).toBe('object')
@@ -57,15 +17,123 @@ describe('_makeStudent', () => {
     expect(student2.name).toBe('Sarah');
   })
 
-  it(`creates an array for the partners field`, () => {
-    const studentPartners1 = _makeStudent().partners;
+  it(`creates an array for the partnerCounts field`, () => {
+    const studentPartners1 = _makeStudent().partnerCounts;
 
     expect(Array.isArray(studentPartners1)).toBe(true)
   })
 
-  it(`begins the partners field as an empty array`, () => {
-    const studentPartners1 = _makeStudent().partners;
+  it(`begins the partnerCounts field as an empty array`, () => {
+    const studentPartners1 = _makeStudent().partnerCounts;
 
     expect(studentPartners1.length).toBe(0);
+  })
+})
+
+
+describe('makeRoster', () => {
+  it(`returns an object`, () => {
+    expect(typeof makeRoster()).toBe('object')
+  })
+})
+
+
+describe('roster._students', () => {
+  it(`is an array`, () => {
+    const students = makeRoster()._students
+    expect(Array.isArray(students)).toBe(true)
+  })
+
+  it(`begins as an empty array`, () => {
+    const students = makeRoster()._students;
+    expect(students.length).toBe(0);
+  })
+})
+
+
+describe('roster._groups', () => {
+  it(`is an array`, () => {
+    const groups = makeRoster()._groups
+    expect(Array.isArray(groups)).toBe(true)
+  })
+
+  it(`begins as an empty array`, () => {
+    const groups = makeRoster()._groups;
+    expect(groups.length).toBe(0);
+  })
+})
+
+
+describe('roster.addStudent', () => {
+  it(`is a method`, () => {
+    const roster = makeRoster();
+    expect(typeof roster.addStudent).toBe('function');
+  })
+
+  it(`adds a student with the given name to the roster`, () => {
+    const roster = makeRoster();
+    roster.addStudent('Colin');
+    expect(typeof roster._students[0]).toBe('object')
+    expect(roster._students[0].name).toBe('Colin');
+  })
+
+  it(`uses _makeStudent`, () => {
+    const roster = makeRoster();
+    roster.addStudent('Colin');
+    expect(roster._students).toEqual([_makeStudent('Colin')]);
+  })
+
+  it(`adds the student at the end of the roster`, () => {
+    const roster = makeRoster();
+    roster.addStudent('Colin');
+    expect(roster._students[0].name).toBe('Colin');
+    roster.addStudent('Maya');
+    expect(roster._students[1].name).toBe('Maya');
+  })
+
+  it(`refuses to add an identically-named student`, () => {
+    const roster = makeRoster();
+    roster.addStudent('Maya');
+    expect(() => roster.addStudent('Maya')).toThrow();
+  })
+
+  it(`refuses to add an unnamed student`, () => {
+    const roster = makeRoster();
+    expect(() => roster.addStudent('')).toThrow();
+  })
+})
+
+
+describe('roster.removeStudent', () => {
+  it(`is a method`, () => {
+    const roster = makeRoster();
+    expect(typeof roster.removeStudent).toBe('function');
+  })
+
+  it(`refuses to remove a student who's not there`, () => {
+    const roster = makeRoster();
+    roster.addStudent('Ian');
+    expect(() => roster.removeStudent('Sal')).toThrow();
+  })
+
+  it(`removes the student with the given name from the roster`, () => {
+    const roster = makeRoster();
+    roster.addStudent('Colin');
+    roster.removeStudent('Colin');
+    expect(roster._students).toEqual([]);
+
+    roster.addStudent('Maya');
+    roster.addStudent('Colin');
+    roster.addStudent('Ian');
+    roster.addStudent('Sal');
+    roster.removeStudent('Colin');
+    expect(roster._students).toEqual([
+      _makeStudent('Maya'),
+      _makeStudent('Ian'),
+      _makeStudent('Sal'),
+    ]);
+    roster.removeStudent('Maya');
+    roster.removeStudent('Sal');
+    expect(roster._students).toEqual([_makeStudent('Ian')]);
   })
 })
